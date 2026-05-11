eef4cc18ed0e963b847d0b5dc7ce9e355293ab0e
Sifiso Maye <sifmay98@gmail.com>
2026-05-11 08:37:13 +0200

    added an address to restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_05_11_062735) do
+ActiveRecord::Schema[7.1].define(version: 2026_05_11_063241) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
