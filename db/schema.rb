3e7d088b4d3148e74a717ccb03e0645894a12188
Sifiso Maye <sifmay98@gmail.com>
2026-05-15 08:46:50 +0200

    finished create and new

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_05_11_063241) do
+ActiveRecord::Schema[7.1].define(version: 2026_05_12_073319) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+    t.string "category"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,68 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+restaurants = [
+  {
+    name: "Ocean Breeze Grill",
+    address: "12 Beach Road, Cape Town",
+    category: "seafood"
+  },
+  {
+    name: "Mama Mia Pizzeria",
+    address: "45 Long Street, Cape Town",
+    category: "italian"
+  },
+  {
+    name: "Golden Dragon",
+    address: "88 Main Road, Johannesburg",
+    category: "chinese"
+  },
+  {
+    name: "Spice Route",
+    address: "23 Victoria Street, Durban",
+    category: "indian"
+  },
+  {
+    name: "Burger Haven",
+    address: "101 Market Avenue, Pretoria",
+    category: "american"
+  },
+  {
+    name: "Sakura Sushi",
+    address: "9 River Lane, Cape Town",
+    category: "japanese"
+  },
+  {
+    name: "Le Petit Cafe",
+    address: "17 Church Square, Stellenbosch",
+    category: "french"
+  },
+  {
+    name: "Taco Fiesta",
+    address: "56 Palm Street, Port Elizabeth",
+    category: "mexican"
+  },
+  {
+    name: "Green Garden Vegan",
+    address: "73 Forest Drive, Cape Town",
+    category: "vegan"
+  },
+  {
+    name: "Braai House",
+    address: "200 Freedom Blvd, Johannesburg",
+    category: "south african"
+  }
+]
+
+restaurants.each do |restaurant|
+  Restaurant.create!(restaurant)
+end
+
+puts "#{Restaurant.count} restaurants created!"
