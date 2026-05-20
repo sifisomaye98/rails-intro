ffd63bd3057ff933900e867de086891086f5227f
Sifiso Maye <sifmay98@gmail.com>
2026-05-20 12:13:54 +0200

    reviews section

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -20,5 +21,6 @@ Rails.application.routes.draw do
     collection do
       get :top
     end
+    resources :reviews, only: [:new, :create]
   end
 end
