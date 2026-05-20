4bece066029261cd61b27c30c6f157c7e382a93b
Sifiso Maye <sifmay98@gmail.com>
2026-05-20 12:35:25 +0200

    done reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,6 +16,12 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant), status: :see_other
+  end
+
   private
 
   def set_restaurant
--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,5 @@ Rails.application.routes.draw do
     end
     resources :reviews, only: [:new, :create]
   end
+  resources :reviews, only: [:destroy]
 end
